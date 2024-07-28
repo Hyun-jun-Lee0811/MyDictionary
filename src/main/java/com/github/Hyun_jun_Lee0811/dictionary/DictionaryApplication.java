9573c18bc71a1a9bfbf2469cdd16aaa8d389d0ab
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-07-28 16:24:58 +0900

    Project Setting

---
+++ src/main/java/com/github/Hyun_jun_Lee0811/dictionary/DictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.github.Hyun_jun_Lee0811.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Hyun_jun_Lee0811/dictionary/DictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Hyun_jun_Lee0811.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
