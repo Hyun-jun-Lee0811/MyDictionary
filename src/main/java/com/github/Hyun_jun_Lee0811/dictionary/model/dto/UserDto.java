ef8b04801ff4e2d5d062cfeee0bbb4524f4e874e
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-08-05 20:42:01 +0900

    chore: DTO의불필요한import제거

--- src/main/java/com/github/Hyun_jun_Lee0811/dictionary/model/dto/UserDto.java
+++ src/main/java/com/github/Hyun_jun_Lee0811/dictionary/model/dto/UserDto.java
@@ -1,9 +1,6 @@
 package com.github.Hyun_jun_Lee0811.dictionary.model.dto;
 
-
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
