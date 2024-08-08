805f754164c20a18e1b143de7032349516b00e7d
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-08-08 20:22:07 +0900

    refactor: 대소문자 구분 없는 검색 기능으로 변경 - Trie 기반의 검색 기능에서 대소문자 구분을 제거했습니다.

--- src/main/java/com/github/Hyun_jun_Lee0811/dictionary/service/WordService.java
+++ src/main/java/com/github/Hyun_jun_Lee0811/dictionary/service/WordService.java
@@ -16,42 +16,47 @@ public class WordService {
   private final Trie<String, Boolean> trie;
 
   public List<WordDefinitionDto> getDefinitions(String word) {
-    List<WordDefinitionDto> definitions = wordnikClient.getDefinitions(word);
-    updateTrieWithWord(word);
+    List<WordDefinitionDto> definitions = wordnikClient.getDefinitions(normalizeWord(word));
+    updateTrie(normalizeWord(word));
     return definitions;
   }
 
   public WordExampleDto getExamples(String word) {
-    WordExampleDto examples = wordnikClient.getExamples(word);
-    updateTrieWithWord(word);
+    WordExampleDto examples = wordnikClient.getExamples(normalizeWord(word));
+    updateTrie(normalizeWord(word));
     return examples;
   }
 
   public List<WordRelatedWordDto> getRelatedWords(String word) {
-    List<WordRelatedWordDto> relatedWords = wordnikClient.getRelatedWords(word);
-    updateTrieWithWord(word);
+    List<WordRelatedWordDto> relatedWords = wordnikClient.getRelatedWords(normalizeWord(word));
+    updateTrie(normalizeWord(word));
     return relatedWords;
   }
 
   public List<WordPronunciationDto> getPronunciations(String word) {
-    List<WordPronunciationDto> pronunciations = wordnikClient.getPronunciations(word);
-    updateTrieWithWord(word);
+    List<WordPronunciationDto> pronunciations = wordnikClient.getPronunciations(
+        normalizeWord(word));
+    updateTrie(normalizeWord(word));
     return pronunciations;
   }
 
   public List<WordEtymologyDto> getEtymologies(String word) {
-    List<WordEtymologyDto> etymologies = wordnikClient.getEtymologies(word);
-    updateTrieWithWord(word);
+    List<WordEtymologyDto> etymologies = wordnikClient.getEtymologies(normalizeWord(word));
+    updateTrie(normalizeWord(word));
     return etymologies;
   }
 
-  public List<String> autocomplete(String prefix) {
-    return trie.prefixMap(prefix).keySet()
+  public List<String> autocomplete(String word) {
+    return trie.prefixMap(normalizeWord(word)).keySet()
         .stream()
         .toList();
   }
 
-  private void updateTrieWithWord(String word) {
+  private void updateTrie(String word) {
     trie.put(word, Boolean.TRUE);
+  }
+
+  private String normalizeWord(String word) {
+    return word.toLowerCase();
   }
 }
