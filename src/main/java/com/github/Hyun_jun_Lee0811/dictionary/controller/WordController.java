6b9bb7011b1a775d9262cac39d87455d57ba9535
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-08-08 20:19:53 +0900

    feat: Trie를 이용한 검색 기능 구현

--- src/main/java/com/github/Hyun_jun_Lee0811/dictionary/controller/WordController.java
+++ src/main/java/com/github/Hyun_jun_Lee0811/dictionary/controller/WordController.java
@@ -4,42 +4,50 @@ import com.github.Hyun_jun_Lee0811.dictionary.model.dto.*;
 import com.github.Hyun_jun_Lee0811.dictionary.service.WordService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/word.json/{word}")
+@RequestMapping("/word.json")
 @RequiredArgsConstructor
 public class WordController {
 
   private final WordService wordService;
 
-  @GetMapping("/definitions")
+  @GetMapping("/{word}/definitions")
   public List<WordDefinitionDto> getDefinitions(@Valid @PathVariable("word") String word) {
     return wordService.getDefinitions(word);
   }
 
-  @GetMapping("/examples")
+  @GetMapping("/{word}/examples")
   public WordExampleDto getExamples(@Valid @PathVariable("word") String word) {
     return wordService.getExamples(word);
   }
 
-  @GetMapping("/relatedWords")
+  @GetMapping("/{word}/relatedWords")
   public List<WordRelatedWordDto> getRelatedWords(@Valid @PathVariable("word") String word) {
     return wordService.getRelatedWords(word);
   }
 
-  @GetMapping("/pronunciations")
+  @GetMapping("/{word}/pronunciations")
   public List<WordPronunciationDto> getPronunciations(@Valid @PathVariable("word") String word) {
     return wordService.getPronunciations(word);
   }
 
-  @GetMapping("/etymologies")
+  @GetMapping("/{word}/etymologies")
   public List<WordEtymologyDto> getEtymologies(@Valid @PathVariable("word") String word) {
     return wordService.getEtymologies(word);
+  }
+
+  @GetMapping("/autocomplete")
+  public ResponseEntity<List<String>> autocomplete(@Valid @RequestParam("word") String word) {
+    List<String> suggestions = wordService.autocomplete(word);
+    return ResponseEntity.ok(suggestions);
   }
 }
--- src/main/java/com/github/Hyun_jun_Lee0811/dictionary/service/WordService.java
+++ src/main/java/com/github/Hyun_jun_Lee0811/dictionary/service/WordService.java
@@ -3,6 +3,7 @@ package com.github.Hyun_jun_Lee0811.dictionary.service;
 import com.github.Hyun_jun_Lee0811.dictionary.client.WordnikClient;
 import com.github.Hyun_jun_Lee0811.dictionary.model.dto.*;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.Trie;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,24 +13,45 @@ import java.util.List;
 public class WordService {
 
   private final WordnikClient wordnikClient;
+  private final Trie<String, Boolean> trie;
 
   public List<WordDefinitionDto> getDefinitions(String word) {
-    return wordnikClient.getDefinitions(word);
+    List<WordDefinitionDto> definitions = wordnikClient.getDefinitions(word);
+    updateTrieWithWord(word);
+    return definitions;
   }
 
   public WordExampleDto getExamples(String word) {
-    return wordnikClient.getExamples(word);
+    WordExampleDto examples = wordnikClient.getExamples(word);
+    updateTrieWithWord(word);
+    return examples;
   }
 
   public List<WordRelatedWordDto> getRelatedWords(String word) {
-    return wordnikClient.getRelatedWords(word);
+    List<WordRelatedWordDto> relatedWords = wordnikClient.getRelatedWords(word);
+    updateTrieWithWord(word);
+    return relatedWords;
   }
 
   public List<WordPronunciationDto> getPronunciations(String word) {
-    return wordnikClient.getPronunciations(word);
+    List<WordPronunciationDto> pronunciations = wordnikClient.getPronunciations(word);
+    updateTrieWithWord(word);
+    return pronunciations;
   }
 
   public List<WordEtymologyDto> getEtymologies(String word) {
-    return wordnikClient.getEtymologies(word);
+    List<WordEtymologyDto> etymologies = wordnikClient.getEtymologies(word);
+    updateTrieWithWord(word);
+    return etymologies;
+  }
+
+  public List<String> autocomplete(String prefix) {
+    return trie.prefixMap(prefix).keySet()
+        .stream()
+        .toList();
+  }
+
+  private void updateTrieWithWord(String word) {
+    trie.put(word, Boolean.TRUE);
   }
 }
