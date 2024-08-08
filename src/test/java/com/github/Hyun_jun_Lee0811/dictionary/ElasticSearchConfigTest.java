51f14e13d2a3917aa02537bef1a4c3da5cb0b554
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-08-08 16:51:21 +0900

    test: Elasticsearch 클라이언트 구성 검증 테스트

---
+++ src/test/java/com/github/Hyun_jun_Lee0811/dictionary/ElasticSearchConfigTest.java
@@ -0,0 +1,22 @@
+package com.github.Hyun_jun_Lee0811.dictionary;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class ElasticSearchConfigTest {
+
+  @Autowired
+  private ElasticsearchClient elasticsearchClient;
+
+  @Test
+  @DisplayName("Elasticsearch 클라이언트 구성 검증")
+  void contextLoads() {
+    assertNotNull(elasticsearchClient, "Elasticsearch 클라이언트가 구성되어 있으며 null이 아닙니다.");
+  }
+}
