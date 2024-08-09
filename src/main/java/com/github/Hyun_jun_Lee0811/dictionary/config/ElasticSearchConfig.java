2e0fff821a43d4b6a5f290d19e02a7ef8a2e5a98
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-08-09 21:44:42 +0900

    refactor: Elasticsearch 클러스터와의 연결 시 자격 증명을 추가

--- src/main/java/com/github/Hyun_jun_Lee0811/dictionary/config/ElasticSearchConfig.java
+++ src/main/java/com/github/Hyun_jun_Lee0811/dictionary/config/ElasticSearchConfig.java
@@ -3,6 +3,9 @@ package com.github.Hyun_jun_Lee0811.dictionary.config;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.apache.http.HttpHost;
@@ -16,11 +19,25 @@ public class ElasticSearchConfig {
   @Value("${aws.elasticsearch.url}")
   private String url;
 
+  @Value("${aws.elasticsearch.username}")
+  private String username;
+
+  @Value("${aws.elasticsearch.password}")
+  private String password;
+
   @Bean
   public ElasticsearchClient elasticsearchClient() {
-    RestClientBuilder builder = RestClient.builder(HttpHost.create(url));
-    RestClient Client = builder.build();
-    RestClientTransport transport = new RestClientTransport(Client, new JacksonJsonpMapper());
+    BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    credentialsProvider.setCredentials(
+        AuthScope.ANY,
+        new UsernamePasswordCredentials(username, password));
+
+    RestClientBuilder builder = RestClient.builder(HttpHost.create(url))
+        .setHttpClientConfigCallback(httpClientBuilder ->
+            httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+    RestClient client = builder.build();
+    RestClientTransport transport = new RestClientTransport(client, new JacksonJsonpMapper());
 
     return new ElasticsearchClient(transport);
   }
