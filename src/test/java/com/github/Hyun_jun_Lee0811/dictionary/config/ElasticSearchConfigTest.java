c444a1be9a8f6e9dd291578344b2eb6eecd3006b
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-08-09 21:45:25 +0900

    test: Elasticsearch 클러스터와의 연결 테스트 추기

--- src/test/java/com/github/Hyun_jun_Lee0811/dictionary/config/ElasticSearchConfigTest.java
+++ src/test/java/com/github/Hyun_jun_Lee0811/dictionary/config/ElasticSearchConfigTest.java
@@ -1,6 +1,7 @@
 package com.github.Hyun_jun_Lee0811.dictionary.config;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.cluster.HealthResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,4 +20,17 @@ class ElasticSearchConfigTest {
   void contextLoads() {
     assertNotNull(elasticsearchClient, "Elasticsearch 클라이언트가 구성되어 있으며 null이 아닙니다.");
   }
+
+  @Test
+  @DisplayName("Elasticsearch 클러스터와의 연결")
+  public void testClusterHealth() {
+    try {
+      HealthResponse healthResponse = elasticsearchClient.cluster().health();
+      System.out.println(healthResponse);
+    } catch (Exception e) {
+      System.err.println("Elasticsearch 클러스터와의 연결 실패");
+      e.printStackTrace();
+    }
+  }
+
 }
