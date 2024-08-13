a3fbe3a176ece290329c437f0a115246341cdbca
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-08-13 16:53:18 +0900

    feat : UserThink 기능의 에러 기능 추가

--- src/main/java/com/github/Hyun_jun_Lee0811/dictionary/type/ErrorCode.java
+++ src/main/java/com/github/Hyun_jun_Lee0811/dictionary/type/ErrorCode.java
@@ -17,7 +17,12 @@ public enum ErrorCode {
   INVALID_JWT_TOKEN("JWT 토큰이 잘못되었습니다."),
   EXAMPLES_API_CLIENT_ERROR("API 호출 중 클라이언트 오류가 발생했습니다."),
   EXAMPLES_API_SERVER_ERROR("API 호출 중 서버 오류가 발생했습니다."),
-  EXAMPLES_API_NETWORK_ERROR("API 호출 중 네트워크 오류가 발생했습니다.");
-
+  EXAMPLES_API_NETWORK_ERROR("API 호출 중 네트워크 오류가 발생했습니다."),
+  USER_ID_NOT_PROVIDED("사용자 아이디가 제공되지 않았습니다."),
+  USER_NOT_AUTHENTICATED("인증된 사용자가 아닙니다"),
+  NO_USERTHINKS_FOUND_OR_ACCESS_DENIED("사용자의 생각을 찾을 수 없거나 접근이 거부되었습니다."),
+  INVALID_USERNAME("잘못된 사용자 이름 입니다."),
+  SERVICE_EXCEPTION("서비스에서 오류가 발생했습니다."),
+  FAILED_SAVE("저장에 실패했습니다");
   private final String message;
 }
