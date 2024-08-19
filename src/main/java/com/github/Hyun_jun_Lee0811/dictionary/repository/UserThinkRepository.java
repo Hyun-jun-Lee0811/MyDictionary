d88beac0e2b45ad2c903e4d863a904e8ef82ff70
Hyun-jun-Lee0811 <korong0811@naver.com>
2024-08-19 17:20:54 +0900

    refactor: @Repository 어노테이션 추가

--- src/main/java/com/github/Hyun_jun_Lee0811/dictionary/repository/UserThinkRepository.java
+++ src/main/java/com/github/Hyun_jun_Lee0811/dictionary/repository/UserThinkRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserThinkRepository extends JpaRepository<UserThink, Long> {
 
   List<UserThink> findByUserIdAndIsPrivate(Long userId, Boolean isPrivate);
